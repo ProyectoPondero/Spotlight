7450501e1782872edfa3c4469a829f44d37d49e5
dev-CristianD.Gonzalez <cristiand.gonzalez094@gmail.com>
2024-06-20 18:23:04 -0300

    se añadio el metodo de login

--- client/assets/script/logIn.js
+++ client/assets/script/logIn.js
@@ -1,9 +1,10 @@
 const registerForm = document.getElementById('registerForm');
+const logInForm = document.getElementById('logInForm');
 
 // Coneccion con el backend para registrar un usuario
 const registerDataBase = async (data) => {
     try {
-        const res = await fetch('http://localhost:3000/api/auth/register', {
+        const res = await fetch('http://localhost:3000/api/user/register', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -24,14 +25,49 @@ const registerDataBase = async (data) => {
 registerForm.addEventListener('submit', async (event) => {
     event.preventDefault();
     const formData = new FormData(registerForm);
-    const usuario = formData.get('usuario');
+    const userName = formData.get('usuario');
     const email = formData.get('email');
-    const contraseña = formData.get('contraseña');
-    const data = { usuario, email, contraseña };
+    const password = formData.get('contraseña');
+    const data = { userName, email, password };
     const res = await registerDataBase(data);
     if (res) {
         alert('Usuario creado correctamente!');
         return registerForm.reset();
     }
-    alert('Error al crear el usuario');
+    alert('El usuario ya existe!');
+});
+
+// Conexion con el backend para loguear un usuario
+const logInDataBase = async (data) => {
+    try {
+        const res = await fetch('http://localhost:3000/api/user/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+        if (!res.ok) {
+            throw new Error('Error al loguear usuario');
+        }
+        const resData = await res.json();
+        return resData;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// Si las credenciales son correctas, se redirige al usuario a la página principal
+logInForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const formData = new FormData(logInForm);
+    const email = formData.get('email');
+    const password = formData.get('contraseña');
+    const data = { email, password };
+    const res = await logInDataBase(data);
+    if (res) {
+        alert('Usuario logueado correctamente!');
+        return window.location.replace('http://localhost:5500/client/public/index.html');
+    }
+    alert('Usuario o contraseña incorrectos!');
 });
